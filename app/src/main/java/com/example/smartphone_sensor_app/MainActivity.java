6bd86ce5f172306e5305c4bed398b22aa8904669
Shanika-Priyadarshani <shanika.15@cse.mrt.ac.lk>
2019-09-16 11:32:26 +0530

    Adding Styles

--- app/src/main/java/com/example/smartphone_sensor_app/MainActivity.java
+++ app/src/main/java/com/example/smartphone_sensor_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smartphone_sensor_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,13 +14,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager mSensorManager;
-
-    // Individual light and proximity sensors.
-    private Sensor mSensorLight;
-
-    // TextViews to display current sensor values
-    private TextView mTextSensorLight;
+    private SensorManager sensorManager;
+    private Sensor sensorLight;
+    private TextView textSensorLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +28,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onStart() {
         super.onStart();
-        mSensorManager =
+        sensorManager =
                 (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mTextSensorLight = (TextView) findViewById(R.id.label_light);
-        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if (mSensorLight != null) {
-            mSensorManager.registerListener(this, mSensorLight,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-        }
-        String sensor_error = getResources().getString(R.string.error_no_sensor);
-        if (mSensorLight == null) {
-            mTextSensorLight.setText(sensor_error);
-        }
+        textSensorLight = (TextView) findViewById(R.id.label_light);
+        String sensor_stopped = getResources().getString(R.string.no_data);
+        textSensorLight.setText(sensor_stopped);
+        textSensorLight.setTextColor(Color.GRAY);
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mSensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(this);
     }
 
     @Override
@@ -56,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         float currentValue = sensorEvent.values[0];
 
-        mTextSensorLight.setText(getResources().getString(
+        textSensorLight.setText(getResources().getString(
                 R.string.label_light, currentValue));
 
         }
@@ -69,24 +61,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     public void onStartClicked(View view){
-        mSensorManager =
-                (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mTextSensorLight = (TextView) findViewById(R.id.label_light);
-        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if (mSensorLight != null) {
-            mSensorManager.registerListener(this, mSensorLight,
+
+        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (sensorLight != null) {
+            sensorManager.registerListener(this, sensorLight,
                     SensorManager.SENSOR_DELAY_NORMAL);
+            textSensorLight.setTextColor(Color.BLACK);
         }
         String sensor_error = getResources().getString(R.string.error_no_sensor);
-        if (mSensorLight == null) {
-            mTextSensorLight.setText(sensor_error);
+        if (sensorLight == null) {
+            textSensorLight.setText(sensor_error);
         }
+
     }
 
     public void onStopClicked(View view){
-        mSensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(this);
         String sensor_stopped = getResources().getString(R.string.no_data);
-        mTextSensorLight.setText(sensor_stopped);
+        textSensorLight.setText(sensor_stopped);
+        textSensorLight.setTextColor(Color.GRAY);
     }
 
 }
